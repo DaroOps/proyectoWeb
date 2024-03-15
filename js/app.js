29ccf0513aadf68a99f51a74da1d7b2f974ca897
daroOps <romerosd089@hotmail.com>
2024-03-15 01:22:50 -0500

    feat: :poop: Add logic for show the cart items

--- js/app.js
+++ js/app.js
@@ -14,61 +14,187 @@ const buttonShirts = document.getElementById("shirts");
 const buttonPants = document.getElementById("pants");
 const buttonCar = document.getElementById("car");
 
+const carSize = document.getElementById("cart-count");
+
 
 buttonAll.addEventListener("click", function (event) {
-    activateTabIndex(event);
-    view.textContent = "Todos los productos"
     cleanView()
+    activateTabIndex(event);
+    carSize.textContent = car.length
+    view.textContent = "Todos los productos"
+    if (!document.getElementById("product-grid")) {
+        document.getElementById("car-grid").id = "product-grid"
+    }
     createProduct(allProducts)
-    document.getElementById("nothing")?document.getElementById("nothing").remove():null
+    hideElement("car-manager")
+    document.getElementById("nothing") ? document.getElementById("nothing").remove() : null
 });
 
 buttonCoats.addEventListener("click", function (event) {
-    activateTabIndex(event);
-    view.textContent = "Abrigos"
     cleanView()
+    activateTabIndex(event);
+    carSize.textContent = car.length
+    view.textContent = "Abrigos"
+    if (!document.getElementById("product-grid")) {
+        document.getElementById("car-grid").id = "product-grid"
+    }
     createProduct(coats)
-    document.getElementById("nothing")?document.getElementById("nothing").remove():null
+    hideElement("car-manager")
+    document.getElementById("nothing") ? document.getElementById("nothing").remove() : null
 });
 
 buttonShirts.addEventListener("click", function (event) {
-    activateTabIndex(event);
-    view.textContent = "Camisetas"
     cleanView()
+    activateTabIndex(event);
+    carSize.textContent = car.length
+    if (!document.getElementById("product-grid")) {
+        document.getElementById("car-grid").id = "product-grid"
+    }
+    view.textContent = "Camisetas"
     createProduct(tShirths)
-    document.getElementById("nothing")?document.getElementById("nothing").remove():null
+    hideElement("car-manager")
+    document.getElementById("nothing") ? document.getElementById("nothing").remove() : null
 });
 
 buttonPants.addEventListener("click", function (event) {
-    activateTabIndex(event);
-    view.textContent = "Pantalones"
     cleanView()
+    activateTabIndex(event);
+    carSize.textContent = car.length
+    if (!document.getElementById("product-grid")) {
+        document.getElementById("car-grid").id = "product-grid"
+    }
+   
+    view.textContent = "Pantalones"
     createProduct(pants)
-    
-    document.getElementById("nothing")?document.getElementById("nothing").remove():null
+    hideElement("car-manager")
+    document.getElementById("nothing") ? document.getElementById("nothing").remove() : null
 });
 
 buttonCar.addEventListener("click", function (event) {
     cleanView()
-    
+
+
+    if (!document.getElementById("car-grid")) {
+        document.getElementById("product-grid").id = "car-grid"
+    }
+    console.log(car);
+    createCartItem(car)
+    carSize.textContent = car.length
     const empty = document.createElement("h3");
     empty.setAttribute("id", "nothing");
     empty.textContent = "Tu carrito esta vacio :("
-    
+
     activateTabIndex(event);
-    
-   
-    if(checkEmptynes("product-grid")){
-      
+
+    console.log(checkEmptynes("car-grid"));
+
+    if (!checkEmptynes("car-grid")) {
+        showElement("car-manager") 
+        document.getElementById("nothing") ? document.getElementById("nothing").remove() : null
     }
-    else{
-        if(!document.getElementById("nothing")){
+    else {
+        if (!document.getElementById("nothing")) {
             productContainer.appendChild(empty)
         }
         hideElement("car-manager")
     }
     view.textContent = "Carrito"
 });
+
+function createCartItem(itemDataArray) {
+    const carContainer = document.getElementById('car-grid');
+
+    itemDataArray.forEach(itemData => {
+        const carItem = document.createElement('div');
+        carItem.classList.add('car-item');
+        carItem.id = itemData.id;
+
+        let productName = '';
+        let productImage = '';
+        let productPrice = '';
+
+        if (itemData.abrigoId !== undefined) {
+            const coat = coats.find(coat => coat.id === itemData.abrigoId);
+            if (coat) {
+                productName = coat.nombre;
+                productImage = coat.imagen;
+                productPrice = coat.precio;
+            }
+        } else if (itemData.camisetaId !== undefined) {
+            const tShirt = tShirths.find(tShirt => tShirt.id === itemData.camisetaId);
+            if (tShirt) {
+                productName = tShirt.nombre;
+                productImage = tShirt.imagen;
+                productPrice = tShirt.precio;
+            }
+        } else if (itemData.pantalonId !== undefined) {
+            const pant = pants.find(pant => pant.id === itemData.pantalonId);
+            if (pant) {
+                productName = pant.nombre;
+                productImage = pant.imagen;
+                productPrice = pant.precio;
+            }
+        }
+
+        const carImg = document.createElement('div');
+        carImg.classList.add('car-img');
+        const imageElement = document.createElement('img');
+        imageElement.src = productImage;
+        carImg.appendChild(imageElement);
+
+        const carItemName = document.createElement('div');
+        carItemName.classList.add('car-item-name');
+        const nameTitle = document.createElement('h6');
+        nameTitle.textContent = 'Nombre';
+        const nameParagraph = document.createElement('p');
+        nameParagraph.id = 'car-item-name';
+        nameParagraph.textContent = productName;
+        carItemName.appendChild(nameTitle);
+        carItemName.appendChild(nameParagraph);
+
+        const carItemQuantity = document.createElement('div');
+        carItemQuantity.classList.add('car-item-quantity');
+        const quantityTitle = document.createElement('h6');
+        quantityTitle.textContent = 'Cantidad';
+        const quantityParagraph = document.createElement('p');
+        quantityParagraph.id = 'car-item-quantity';
+        quantityParagraph.textContent = itemData.cantidad;
+        carItemQuantity.appendChild(quantityTitle);
+        carItemQuantity.appendChild(quantityParagraph);
+
+        const carItemPrice = document.createElement('div');
+        carItemPrice.classList.add('car-item-price');
+        const priceTitle = document.createElement('h6');
+        priceTitle.textContent = 'Precio';
+        const priceParagraph = document.createElement('p');
+        priceParagraph.id = 'car-item-price';
+        priceParagraph.textContent = "$ "+productPrice;
+        carItemPrice.appendChild(priceTitle);
+        carItemPrice.appendChild(priceParagraph);
+
+        const carItemSubtotal = document.createElement('div');
+        carItemSubtotal.classList.add('car-item-subtotal');
+        const subtotalTitle = document.createElement('h6');
+        subtotalTitle.textContent = 'Subtotal';
+        const subtotalParagraph = document.createElement('p');
+        subtotalParagraph.id = 'car-item-subtotal';
+        subtotalParagraph.textContent = "$ "+itemData.cantidad * productPrice;
+        carItemSubtotal.appendChild(subtotalTitle);
+        carItemSubtotal.appendChild(subtotalParagraph);
+
+        const trashIcon = document.createElement('i');
+        trashIcon.classList.add('bx', 'bxs-trash');
+
+        carItem.appendChild(carImg);
+        carItem.appendChild(carItemName);
+        carItem.appendChild(carItemQuantity);
+        carItem.appendChild(carItemPrice);
+        carItem.appendChild(carItemSubtotal);
+        carItem.appendChild(trashIcon);
+
+        carContainer.appendChild(carItem);
+    });
+}
 
 function createProduct(data) {
     const productsContainer = document.getElementById('product-grid');
@@ -91,13 +217,13 @@ function createProduct(data) {
         const title = document.createElement('h3');
         title.textContent = productData.nombre;
 
-        if(productData.nombre.includes("Camiseta")){
+        if (productData.nombre.includes("Camiseta")) {
             product.classList.add("camiseta")
         }
-        else if(productData.nombre.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes("pantalon")){
+        else if (productData.nombre.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes("pantalon")) {
             product.classList.add("pantalon")
         }
-        else if(productData.nombre.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes("chaqueta")){
+        else if (productData.nombre.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes("chaqueta")) {
             product.classList.add("chaqueta")
         }
 
@@ -111,13 +237,13 @@ function createProduct(data) {
         button.classList.add('add-button')
         button.textContent = "Agregar";
 
-        button.addEventListener("click", function(event){
+        button.addEventListener("click", function (event) {
             console.log("click add");
-            alert(event.target.parentNode.parentNode.id);
+            console.log(event.target.parentNode.parentNode.parentNode);
 
             // event.target.
-           
-        }); 
+
+        });
 
         productImg.appendChild(productImgSrc);
         productInfo.append(title, infoFoo);
@@ -159,8 +285,10 @@ async function fetchData() {
                 }
             }
         }
+        allProducts.reverse()
         createProduct(allProducts)
         hideElement("car-manager")
+        carSize.textContent = car.length
 
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -182,30 +310,35 @@ function activateTabIndex(event) {
     listItem.focus();
 }
 
-function checkEmptynes(elementId){
+function checkEmptynes(elementId) {
     const selected = document.getElementById(elementId);
     const item = document.getElementById("product-item");
     const carItem = document.getElementById("cart-item");
-    
-    return selected.contains(item);
-}   
 
-function hideElement(elementId){
-    const selected = document.getElementById(elementId);
-
-    selected.style.display= 'none';
+    return selected.contains(item) || selected.contains(carItem);
 }
 
-function showElement(elementId){
+function hideElement(elementId) {
     const selected = document.getElementById(elementId);
 
-    selected.style.display= 'block';
+    selected.style.display = 'none';
+}
+
+function showElement(elementId) {
+    const selected = document.getElementById(elementId);
+
+    selected.style.display = 'flex';
 }
 
 function cleanView() {
     const products = document.querySelectorAll('.product-item');
     products.forEach(product => {
         product.remove();
+    });
+
+    const cars = document.querySelectorAll('.car-item');
+    cars.forEach(car => {
+        car.remove();
     });
 }
 
